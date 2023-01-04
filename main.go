85b18a26a8872e2a3db3b73f3472e47cc0bd22da
theovino99 <theovinoiskandar@gmail.com>
2023-01-05 06:22:14 +0700

    day-8

--- main.go
+++ main.go
@@ -3,24 +3,47 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
 var Data = map[string]interface{}{
 	"Title": "Personal Web",
+	// "IsLogin": true,
 }
+
+type dataProject struct {
+	ID          int
+	Name        string
+	Start       string
+	End         string
+	Description string
+	Tech        []string
+	Duration    string
+}
+
+var Projects = []dataProject{}
 
 func main() {
 	route := mux.NewRouter()
 
 	route.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 
+	// Misc Page
 	route.HandleFunc("/", index).Methods("GET")
-	route.HandleFunc("/project", addproject).Methods("GET")
 	route.HandleFunc("/contact", contact).Methods("GET")
-	route.HandleFunc("/detail-project", detailproject).Methods("GET")
+
+	// Project
+	route.HandleFunc("/project", projectAdd).Methods("GET")
+	route.HandleFunc("/project", projectPost).Methods("POST")
+	route.HandleFunc("/project/{id}", projectDetail).Methods("GET")
+	route.HandleFunc("/project/delete/{id}", projectDelete).Methods("GET")
+	route.HandleFunc("/project/edit/{id}", projectEdit).Methods("GET")
+	route.HandleFunc("/project/edit/{id}", projectEditPost).Methods("POST")
 
 	fmt.Println("server running at localhost:5000")
 	http.ListenAndServe("localhost:5000", route)
@@ -28,48 +51,233 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// var tmpl, err = template.ParseFiles("views/index.html")
+
 	var tmpl, err = template.ParseFiles("views/index.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("message :" + err.Error()))
 		return
 	}
+
+	respData := map[string]interface{}{
+		"Data":     Data,
+		"Projects": Projects,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, respData)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var tmpl, err = template.ParseFiles("views/contact.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message :" + err.Error()))
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
 	tmpl.Execute(w, Data)
 }
 
-func addproject(w http.ResponseWriter, r *http.Request) {
+func projectAdd(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
 	var tmpl, err = template.ParseFiles("views/project.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("message :" + err.Error()))
 		return
 	}
+
 	w.WriteHeader(http.StatusOK)
 	tmpl.Execute(w, Data)
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func projectPost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ProjName := r.PostForm.Get("project-name")
+	ProjStart := r.PostForm.Get("project-start")
+	ProjEnd := r.PostForm.Get("project-end")
+	ProjDescription := r.PostForm.Get("project-description")
+	ProjTech := r.Form["project-tech"]
+	ProjDuration := getDuration(ProjStart, ProjEnd)
+
+	var Project = dataProject{
+		Name:        ProjName,
+		Start:       ProjStart,
+		End:         ProjEnd,
+		Description: ProjDescription,
+		Tech:        ProjTech,
+		Duration:    ProjDuration,
+	}
+
+	fmt.Println("Project Name : ", Project.Name)
+	fmt.Println("Start Date   : ", Project.Start)
+	fmt.Println("End Date     : ", Project.End)
+	fmt.Println("Duration     : ", Project.Duration)
+	fmt.Println("Description  : ", Project.Description)
+	fmt.Println("Technologies : ", Project.Tech)
+	fmt.Println("================================")
+
+	Projects = append(Projects, Project)
+	http.Redirect(w, r, "/project", http.StatusMovedPermanently)
+}
+
+func projectDetail(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	var tmpl, err = template.ParseFiles("views/contact-form.html")
+
+	var tmpl, err = template.ParseFiles("views/project-detail.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("message :" + err.Error()))
 		return
 	}
-	w.WriteHeader(http.StatusOK)
-	tmpl.Execute(w, Data)
-}
 
-func detailproject(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	var tmpl, err = template.ParseFiles("views/detail-project.html")
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("message :" + err.Error()))
 		return
 	}
+
+	// Project := dataProject
+	var Project dataProject
+	for _, data := range Projects {
+		if data.ID == id {
+			Project = data
+			break
+		}
+	}
+
+	DataDetail := map[string]interface{}{
+		"Data":    Data,
+		"Project": Project,
+	}
+
 	w.WriteHeader(http.StatusOK)
-	tmpl.Execute(w, Data)
+	tmpl.Execute(w, DataDetail)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func projectEdit(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var tmpl, err = template.ParseFiles("views/project-edit.html")
+
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message :" + err.Error()))
+		return
+	}
+
+	// Project := dataProject
+	var Project dataProject
+	for _, data := range Projects {
+		if data.ID == id {
+			Project = data
+			break
+		}
+	}
+
+	DataDetail := map[string]interface{}{
+		"Data":    Data,
+		"Project": Project,
+	}
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, DataDetail)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func projectDelete(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	Projects = append(Projects[:id], Projects[id+1:]...)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func projectEditPost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ProjName := r.PostForm.Get("project-name")
+	ProjStart := r.PostForm.Get("project-start")
+	ProjEnd := r.PostForm.Get("project-end")
+	ProjDescription := r.PostForm.Get("project-description")
+	ProjTech := r.Form["project-tech"]
+	ProjDuration := getDuration(ProjStart, ProjEnd)
+
+	var Project = dataProject{
+		Name:        ProjName,
+		Start:       ProjStart,
+		End:         ProjEnd,
+		Description: ProjDescription,
+		Tech:        ProjTech,
+		Duration:    ProjDuration,
+	}
+
+	fmt.Println("Project Name : ", Project.Name)
+	fmt.Println("Start Date   : ", Project.Start)
+	fmt.Println("End Date     : ", Project.End)
+	fmt.Println("Duration     : ", Project.Duration)
+	fmt.Println("Description  : ", Project.Description)
+	fmt.Println("Technologies : ", Project.Tech)
+	fmt.Println("================================")
+
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message :" + err.Error()))
+		return
+	}
+
+	Projects[id] = Project
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+func getDuration(start, end string) string {
+
+	// Store Date with the Format
+	DataStart, _ := time.Parse("2006-01-02", start)
+	DataEnd, _ := time.Parse("2006-01-02", end)
+
+	// Get data Range
+	DataRange := DataEnd.Sub(DataStart)
+
+	// Calc duration
+	yearRange := int(DataRange.Hours() / (12 * 30 * 24))
+	monthRange := int(DataRange.Hours() / (30 * 24))
+	weekRange := int(DataRange.Hours() / (7 * 24))
+	dayRange := int(DataRange.Hours() / 24)
+
+	if yearRange != 0 {
+		return "Duration - " + strconv.Itoa(yearRange) + " Year"
+	}
+	if monthRange != 0 {
+		return "Duration - " + strconv.Itoa(monthRange) + " Month"
+	}
+	if weekRange != 0 {
+		return "Duration - " + strconv.Itoa(weekRange) + " Week Left"
+	}
+	if dayRange != 0 {
+		return "Duration - " + strconv.Itoa(dayRange) + " Day Left"
+	}
+	return "Duration - Today"
 }
